69d8316b9f1f4048232fba5f7097b119130e412a
Gyusun Yeom <omniavinco@gmail.com>
2016-08-01 01:06:57 +0900

    shorten heart

--- heart.c
+++ heart.c
@@ -1,15 +1,11 @@
-
-     #include<stdio.h>          /*code by perlmint*/
-  #define _(a,b) for (int    j=0; j<a;++j){putchar(b);}
-typedef unsigned char uc_;  void pc(char a){ putchar(a);}
-const char _$=' ',_$$='*',_$_='\n';uc_ k[] = {6 , 3 ,1,0}
- ,d=18,e=38,f[]={0,1,3,6,9,13,16,18}; int main(){for(int
-    i = 0; i < 4; ++i) {_(k[i],_$); _(d-k[i]*2, _$$);
-       ;_(k[i]*2+2,_$);_(d-k[i]* 2, _$$);pc(_$_);}
-          for(int i =0;i<sizeof(f);++i){for(int
-             j=0;j< f[i]; ++j){putchar(_$);}
-                for(int j=0;j < e-f[i]*2;
-                   j++){putchar(_$$);}
-                      putchar(_$_);
-                        }return d
-                          - d;}
+      z=32,x=42,          c=10,d=18
+    ,e=38,f[]={0,1      ,3,6,9,13,16,
+  18},i,j;_(a,b){for(j=0;j<a;++j){p(b)
+  ;}}p(x){putchar(x);}main(){for(i=0;i
+   <4;++i){_(f[4-i],z);_(d-f[4-i]*2,x
+     );_(f[4-i]*2+2,z);_(d-f[4-i]*2
+        ,x);p(c);}for(i=0;i<8;++
+            i){_(f[i],z);_(e
+               -f[i]*2,x)
+                 ;p(c);
+                   }}
